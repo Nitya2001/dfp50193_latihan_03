b45ce02753bc2a63b1c33bfc73a186c8c0c27f7b
Nitya2001 <81946328+Nitya2001@users.noreply.github.com>
2021-10-04 11:53:23 +0800

    arahan-arahan asas

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>ASAS PHP </h1>
+
+    <?php 
+     // komen satu baris
+     #komen satu baris
+     /*
+     komen banyak baris
+     */
+
+     $namapelajar = 'Nitya';
+
+    echo "<h2>Hello $namapelajar</h2>"; # output
+    $hadir = false;
+    $umur = 20;
+    $nokp = '010120101578';
+   
+   ?>
+   
+</body>
+</html>
